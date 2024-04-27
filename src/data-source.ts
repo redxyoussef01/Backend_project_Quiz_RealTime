e410dcc8d060efc71599ec254566ccbd15755577
Majjiee <128259183+Majjiee@users.noreply.github.com>
2024-04-28 00:01:50 +0100

    Update data-source.ts
    imported my entities Quiz and Question+ test connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { User, Question, Quiz } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -15,3 +15,19 @@ export const AppDataSource = new DataSource({
     migrations: [],
     subscribers: [],
 })
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Connection initialized with database...");
+  })
+  .catch((error) => console.log(error));
+
+export const getDataSource = (delay = 3000): Promise<DataSource> => {
+  if (AppDataSource.isInitialized) return Promise.resolve(AppDataSource);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (AppDataSource.isInitialized) resolve(AppDataSource);
+      else reject("Failed to create connection with database");
+    }, delay);
+  });
+};
