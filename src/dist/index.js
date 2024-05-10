fd83d9d3b5a5f3cabee47b10297f84da027d41df
redxyoussef01 <yourais00@gmail.com>
2024-05-10 14:15:53 +0100

    new entity

--- src/dist/index.js
+++ src/dist/index.js
@@ -2,10 +2,16 @@
 exports.__esModule = true;
 var data_source_1 = require("./data-source");
 var express = require("express");
-var routes = require("./routes.ts");
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+var routes = require("./routes.ts");
+var cors = require("cors");
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: ["Content-Type"]
+}));
 routes(app, data_source_1.AppDataSource);
 app.listen(4000, function () {
     console.log("server running on 4000");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { AppDataSource } from "./data-source";
 
 import * as express from "express";
-
-const routes = require("./routes.ts");
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const routes = require("./routes.ts");
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: ["Content-Type"],
+  })
+);
 
 routes(app, AppDataSource);
 
